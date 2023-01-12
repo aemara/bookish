a1d79999fd1725cd25020c5183ea63f20ee9e5ad
Amr Emara <a.emara47@gmail.com>
2023-01-12 11:38:28 +0200

    add icons to header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faHeart, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 
 export default function Header() {
@@ -9,11 +8,11 @@ export default function Header() {
     <header className="border-b-2 pb-4">
       <div className="bg-slate-300 text-slate-800 py-2">
         <div className="w-5/6 text-sm tracking-wider mx-auto flex gap-x-3">
-          <a href="/" className="font-light uppercase">
+          <a href="/" className="font-light uppercase hover:text-slate-500 transition">
             sign in
           </a>
           <span>|</span>
-          <a href="/" className="font-light uppercase">
+          <a href="/" className="font-light uppercase hover:text-slate-500 transition">
             sign up
           </a>
         </div>
@@ -24,24 +23,27 @@ export default function Header() {
           <h1 className="text-white bg-slate-800 p-2  text-5xl tracking-wider font-black">
             bookish
           </h1>
-          <div className="">
+          <div>
             <input
               type="text"
               placeholder="Search our books"
               className="border
             border-slate-500 hover:border-slate-900 focus:outline-none focus:border-slate-900 active:border-slate-900 px-2 py-2 mr-1"
             />
-            <button class="bg-slate-900 p-2 hover:bg-slate-600"><FontAwesomeIcon icon={faMagnifyingGlass} size="xl" color="white"/></button>
+            <button className="bg-slate-900 p-2 hover:bg-slate-600 transition mr-7"><FontAwesomeIcon icon={faMagnifyingGlass} size="xl" color="white"/></button>
+            <button className="mr-5"><FontAwesomeIcon icon={faHeart} size="2xl" /></button>
+            <button><FontAwesomeIcon icon={faCartShopping} size="2xl" /></button>
+
           </div>
         </div>
       </div>
 
       <nav className="w-5/6 mx-auto mt-2">
         <ul className="flex justify-between text-slate-900 font-medium text-l">
-          <li className="hover:text-slate-500 hover:cursor-pointer">shop books</li>
-          <li className="hover:text-slate-500 hover:cursor-pointer">NYT best sellers</li>
-          <li className="hover:text-slate-500 hover:cursor-pointer">project gutenberg</li>
-          <li className="hover:text-slate-500 hover:cursor-pointer">open library</li>
+          <li className="hover:text-slate-500 hover:cursor-pointer transition">shop books</li>
+          <li className="hover:text-slate-500 hover:cursor-pointer transition">NYT best sellers</li>
+          <li className="hover:text-slate-500 hover:cursor-pointer transition">project gutenberg</li>
+          <li className="hover:text-slate-500 hover:cursor-pointer transition">open library</li>
         </ul>
       </nav>
     </header>
