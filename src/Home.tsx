2ef8a99b5b7f043372ceab93a6bf684c025ef71e
Amr Emara <a.emara47@gmail.com>
2023-01-23 18:01:21 +0200

    add hero and latestBooks in home component

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./hero/Hero";
+import LatestBooks from "./latest/LatestBooks";
+
+export default function Home() {
+  return (
+    <div>
+      <Hero></Hero>
+      <LatestBooks></LatestBooks>
+    </div>
+  );
+}
